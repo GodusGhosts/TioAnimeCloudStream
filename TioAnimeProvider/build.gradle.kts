6ddaa7572796d26f4e8f1874fa43cec5c6410e13
GodusGhosts <gruiz172@gmail.com>
2026-05-19 09:53:34 -0400

    Refactor build.gradle.kts for TioAnimeProvider

--- TioAnimeProvider/build.gradle.kts
+++ TioAnimeProvider/build.gradle.kts
@@ -1,27 +1,11 @@
-plugins {
-    id("com.android.library")
-    kotlin("android")
-}
-
-apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-android {
-    namespace = "com.godusghosts.tioanime"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 21
-    }
-}
+version = 1
 
 cloudstream {
     language = "es"
     authors = listOf("GodusGhosts")
     status = 1
 
-    tvTypes = listOf(
-        "Anime"
-    )
+    tvTypes = listOf("Anime")
 
     description = "Anime sub español latino desde TioAnime"
     iconUrl = "https://tioanime.com/favicon.ico"
