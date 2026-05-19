e87b97055e8c1bdefe1179e245bf3f3b985aa840
GodusGhosts <gruiz172@gmail.com>
2026-05-19 07:58:53 -0400

    Add initial build.gradle.kts configuration

---
+++ TioAnimeProvider/build.gradle.kts
@@ -0,0 +1,15 @@
+version = 1
+
+cloudstream {
+    language = "es"
+    description = "Anime sub español latino desde TioAnime"
+    authors = listOf("GodusGhosts")
+
+    status = 1
+
+    tvTypes = listOf(
+        "Anime"
+    )
+
+    iconUrl = "https://tioanime.com/favicon.ico"
+}
