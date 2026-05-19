e8c4d6a5bb9bc8aaa79b9a32f6a0fcb80bce8365
GodusGhosts <gruiz172@gmail.com>
2026-05-19 09:45:50 -0400

    Update build.gradle.kts for TioAnimeProvider

--- TioAnimeProvider/build.gradle.kts
+++ TioAnimeProvider/build.gradle.kts
@@ -1,15 +1,28 @@
-version = 1
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+android {
+    namespace = "com.godusghosts.tioanime"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+}
 
 cloudstream {
     language = "es"
-    description = "Anime sub español latino desde TioAnime"
     authors = listOf("GodusGhosts")
-
     status = 1
 
     tvTypes = listOf(
         "Anime"
     )
 
+    description = "Anime sub español latino desde TioAnime"
     iconUrl = "https://tioanime.com/favicon.ico"
 }
